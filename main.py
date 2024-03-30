bc3df4c08bb0fbc0532d651c47b7d4e2e6761331
ktunchar <maikittitee1615@gmail.com>
2024-03-31 03:54:17 +0700

    chore: refactor

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import socket
 
+def ft_open(clientSocket, args):
+	clientSocket.connect((args[0], int(args[1])))
+	response = clientSocket.recv(1024).decode()
+	print("Connected to test.rebex.net.")
+	print(response)
+	username = input("Name: ")
+	clientSocket.send(("USER " + username + "\r\n").encode())
+	response = clientSocket.recv(1024).decode()
+	password = input(response)
+	clientSocket.send(("PASS " + password + "\r\n").encode())
+	response = clientSocket.recv(1024).decode()
+	print(response)
+
+def	ft_close(clientSocket):
+	clientSocket.send("QUIT\r\n".encode())
+	print(clientSocket.recv(1024).decode())
+	clientSocket.close()
+
+
 def	main():
 	while (True):
 		args = input("miniftp> ").strip().split()
 		cmd = args[0]
 		args = args[1:]
+		clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		if (cmd == "quit"):
-			# disconnect 
-			exit (0)
+			break 
 
 		if (cmd == "open"):
-			clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			clientSocket.connect((args[0], int(args[1])))
-			response = clientSocket.recv(1024).decode()
-			print("Connected to test.rebex.net.")
-			print(response)
-			username = input("Name: ")
-			clientSocket.send(("USER " + username + "\r\n").encode())
-			response = clientSocket.recv(1024).decode()
-			password = input(response)
-			clientSocket.send(("PASS " + password + "\r\n").encode())
-			response = clientSocket.recv(1024).decode()
-			print(response)
+			ft_open(clientSocket, args)
 		
 		if (cmd == "disconnect"):
-			clientSocket.send("QUIT\r\n".encode())
-			print(clientSocket.recv(1024).decode())
-			clientSocket.close()
+			ft_close(clientSocket)
 			
 main()
